d8fc845dcd0168bcb68aa94fd1562c55b72ce624
nikhilchoukseyy <nikhilranger18@gmail.com>
2026-07-03 09:39:35 +0530

    signin button ui changed

--- client/src/components/GoogleLoginButton.jsx
+++ client/src/components/GoogleLoginButton.jsx
@@ -3,8 +3,11 @@ import { GoogleLogin } from '@react-oauth/google';
 export default function GoogleLoginButton({ onSuccess, onError }) {
   return (
     <GoogleLogin
+      theme="outline"
+      size="large"
       text="signup_with"
       shape="pill"
+      logo_alignment="left"
       onSuccess={async (credentialResponse) => {
         try {
           await onSuccess?.(credentialResponse.credential);
